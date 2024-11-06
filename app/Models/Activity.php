3b2d22546632c206595b5a128d2fe374747dd661
Carlos Moreno <morenort1402@gmail.com>
2024-11-06 20:03:02 +0100

    FEAT Relations beetween tables

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Activity extends Model
 {
     protected $fillable = [
         "type", "notes", "user_id", "datetime"
     ];
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_11_04_144129_create_activities_table.php
+++ database/migrations/2024_11_04_144129_create_activities_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('activities', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
             $table->enum('type', ["surf", "windsurf", "kayak", "atv", "hot air balloon"]);
             $table->dateTime('datetime');
