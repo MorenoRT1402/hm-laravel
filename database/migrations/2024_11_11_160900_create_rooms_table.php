5b7c555b10964cc5cbc1ee22f0f4bcd7fdd13eb3
Carlos Moreno <morenort1402@gmail.com>
2024-11-12 10:13:29 +0100

    FEAT room migration config

---
+++ database/migrations/2024_11_11_160900_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_type');
+            $table->integer('number');
+            $table->string('picture');
+            $table->string('bed_type');
+            $table->string('room_floor');
+            $table->json('facilities');
+            $table->decimal('rate', 10, 2);
+            $table->decimal('discount', 10, 2);
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
